b17ff747e0d5cdf0c06f10ceb62068f7f33f20fc
leoecrepont <leo.ecrepont@mergify.com>
2024-11-26 16:47:02 +0100

    chore: use mergify-cli for upload

--- src/scripts/upload.sh
+++ src/scripts/upload.sh
@@ -1,35 +1,5 @@
 #!/bin/bash
 
-set -x
+set -e -x
 
-if [[ $REPO_URL =~ ^https:\/\/github\.com\/([a-zA-Z0-9._-]+)\/([a-zA-Z0-9._-]+)$ ]]; then
-  REPO_FULL_NAME=${BASH_REMATCH[1]}/${BASH_REMATCH[2]}
-else
-  echo "Invalid repository URL: $REPO_URL"
-  exit 1
-fi
-
-# Check if FILES and TOKEN are set and not empty
-if [ -z "${FILES}" ]; then
-  echo "report_paths is not set or is empty"
-  exit 1
-fi
-if [ -z "${!TOKEN}" ]; then
-  echo "${!TOKEN} is not set or is empty"
-  exit 1
-fi
-
-# TODO: support multiple files
-curl -X POST \
-  -H "Authorization: bearer ${!TOKEN}" \
-  -F name=${CIRCLE_JOB} \
-  -F provider=circleci \
-  -F head_sha=${CIRCLE_SHA1} \
-  -F files=@${FILES} \
-  -o result.json \
-  ${MERGIFY_API_SERVER}/repos/${REPO_FULL_NAME}/ci_issues_upload \
-
-GIGID=$(cat result.json | jq -r .gigid)
-echo "::notice title=CI Issues report::CI_ISSUE_GIGID=$GIGID"
-
-echo "CI_ISSUE_GIGID=$GIGID"
+mergify ci junit-upload "${FILES}"
